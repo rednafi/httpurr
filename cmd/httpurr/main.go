ddd94ae448232b1a75a4345827ec0f678b857d6a
rednafi <redowan.nafi@gmail.com>
2023-08-29 01:09:01 -0400

    Add httpurr -list, refs #8

--- cmd/httpurr/main.go
+++ cmd/httpurr/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"flag"
 	"github.com/rednafi/httpurr/src"
+	"os"
+	"text/tabwriter"
 )
 
 // Ldflags filled by goreleaser
 var version string
 
 func main() {
-	src.Cli(version)
-
+	w := tabwriter.NewWriter(flag.CommandLine.Output(), 0, 4, 4, ' ', 0)
+	src.Cli(w, version, os.Exit)
 }
