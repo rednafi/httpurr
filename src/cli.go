51574d51959f6cea754197e673ad98c4b542efd5
Lucian I. Last <li@last.nl>
2023-08-30 18:20:11 +0200

    add short flags

--- src/cli.go
+++ src/cli.go
@@ -131,9 +131,13 @@ func Cli(w *tabwriter.Writer, version string, exitFunc func(int)) {
 	fs.SetOutput(w)
 
 	code := flag.String("code", "", "Print the description of an HTTP status code")
+	flag.StringVar(code, "c", "", "Print the description of an HTTP status code")
 	help := flag.Bool("help", false, "Print usage")
+	flag.BoolVar(help, "h", false, "Print usage")
 	vers := flag.Bool("version", false, "Print version")
+	flag.BoolVar(vers, "v", false, "Print version")
 	list := flag.Bool("list", false, "Print HTTP status codes")
+	flag.BoolVar(list, "l", false, "Print HTTP status codes")
 	cat := flag.String("cat", "",
 		"Print HTTP status codes by category with -list; \n"+
 			"allowed categories are 1, 2, 3, 4, 5",
